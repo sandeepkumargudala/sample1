cb59db9eddda8c4e7575bb9612534c9165fc3856
sandeepkumargudala <149145028+sandeepkumargudala@users.noreply.github.com>
2023-10-27 13:26:10 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,13 @@
+# Using a for loop to print numbers from 1 to 5
+print("Using a for loop:")
+for i in range(1, 6):  # This loop will iterate from 1 to 5 (inclusive)
+    print(i)
+
+# Using a while loop to calculate the sum of numbers from 1 to 5
+print("\nUsing a while loop:")
+total = 0
+num = 1
+while num <= 5:
+    total += num
+    num += 1
+print("The sum of numbers from 1 to 5 is:", total)
